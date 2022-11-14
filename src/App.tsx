bb6b551ec8d99f6fe90e3d34b31b011665b48c61
Meiyi <ant19950601@gmail.com>
2022-11-15 00:18:09 +0800

    :sparkles: start WritingMode page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import WritingMode from "./pages/writing_mode";
 
 function App() {
   return (
     <div className="App ">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WritingMode />
     </div>
   );
 }
---
+++ src/pages/writing_mode/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WritingMode = () => {
+  return (
+    <div id="WritingMode" className="h-screen">
+      WritingMode
+    </div>
+  );
+};
+
+export default WritingMode;
