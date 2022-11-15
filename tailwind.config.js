29d4aa90e3fdee123602222bcc03a4905c29113f
Meiyi <ant19950601@gmail.com>
2022-11-15 16:53:26 +0800

    :lipstick: update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          FFFFFF: "#FFFFFF",
+        },
+        dark: {
+          E3F6F5: "#E3F6F5",
+        },
+      },
+      dropShadow: {
+        serch: ["0px 1px 2px rgba(0, 0, 0, 0.06)", "0px 1px 3px rgba(0, 0, 0, 0.1)"],
+      },
+      borderRadius: {
+        large: "40px",
+      },
+    },
   },
   plugins: [],
 };
