bdd060970eafce345cbe976c230b3e87d134e3db
Meiyi <ant19950601@gmail.com>
2022-11-16 00:51:42 +0800

    :truck: update sign page and delete scss

---
+++ src/pages/sign/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sign = () => {
+  return (
+    <div className="h-screen bg-no-repeat bg-fixed bg-center bg-cover bg-sign-cover bg-light-blue" >
+      WritingMode
+    </div>
+  );
+};
+
+export default Sign;
