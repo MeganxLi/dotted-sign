5b0c6546a9bcf56be383b9c91052fd092f4a6d1f
Meiyi <ant19950601@gmail.com>
2022-11-16 16:43:58 +0800

    :lipstick: add current page menu li active className

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import routes from "../route/router";
 import { ReactComponent as Menu } from "../assets/svg/menu.svg";
 import { X } from "react-feather";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Head = () => {
   // router
   const navigate = useNavigate();
+  const pathname = useLocation().pathname;
+  const currentPageIdx = routes.findIndex((page) => page.path === pathname);
+
   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
-  const MenuList = (): JSX.Element[] => {
+  const MenuList = (flat: boolean): JSX.Element[] => {
     return routes.map((page, idx) => {
       return (
         <li
           key={idx}
-          className={`flex gap-2 hover:text-blue ${page.classTag} cursor-pointer p-3`}
-          onClick={() => navigate(page.path)}
+          className={`flex gap-2 hover:text-blue ${
+            page.classTag
+          } cursor-pointer p-3 ${currentPageIdx === idx && "active"}`}
+          onClick={() => {
+            navigate(page.path);
+            flat && setOpenMenu(false);
+          }}
         >
           {page.name}
         </li>
@@ -39,7 +47,7 @@ const Head = () => {
         </div>
         <nav className="flex items-center justify-between rounded-large bg-white pr-2 pl-12 shadow-base flat:hidden">
           <ul className="menu grid grid-cols-3 gap-12 text-black/50 ">
-            {MenuList()}
+            {MenuList(false)}
           </ul>
           <div className="rounded-full p-1 outline outline-1 outline-black/20">
             <img src="images/user.png" />
@@ -50,7 +58,7 @@ const Head = () => {
             className="h-full rounded-large bg-white px-4 py-3"
             onClick={() => setOpenMenu(!openMenu)}
           >
-            {openMenu ? <X stroke-width="1" /> : <Menu />}
+            {openMenu ? <X strokeWidth="1" /> : <Menu />}
           </button>
         </div>
       </header>
@@ -64,7 +72,7 @@ const Head = () => {
       >
         {openMenu && (
           <>
-            {MenuList()}
+            {MenuList(true)}
             <div className="mt-4 flex items-center gap-2">
               <div className="rounded-full p-1 outline outline-1 outline-black/20">
                 <img src="images/user.png" />
