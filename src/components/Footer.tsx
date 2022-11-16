9aca55413b48edf8effbbe525a7c9f402ce5729b
Meiyi <ant19950601@gmail.com>
2022-11-16 16:46:00 +0800

    :memo: add footer commpoent

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-white mix-blend-normal">
+      © 2009-2022 Kdan Mobile Software Ltd. All Rights Reserved.
+    </footer>
+  );
+};
+
+export default Footer;
