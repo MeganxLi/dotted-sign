363022bc77feefdb0cf4c96fb8e588ba3ec76254
Meiyi <ant19950601@gmail.com>
2022-11-17 16:42:20 +0800

    :memo: update input component

--- src/components/InputTextField.tsx
+++ src/components/InputTextField.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { ReactComponent as EditIcon } from "../assets/svg/edit.svg";
 
-const InputTextField = () => {
+interface props {
+  InputValue: string;
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputTextField = ({ InputValue, setInputValue }: props) => {
   return (
-    <div className="text-field" >
-      <input type="text"
+    <div className="text-field">
+      <input
+        type="text"
         className="w-full"
+        value={InputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <span><EditIcon className="stroke-black" /></span>
+      <span>
+        <EditIcon className="stroke-black" />
+      </span>
     </div>
   );
 };
