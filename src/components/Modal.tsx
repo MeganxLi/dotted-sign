fe626491e4b58c79282015b20f4b56ac734c8ad2
Meiyi <ant19950601@gmail.com>
2023-01-18 01:03:36 +0800

    :lipstick: add Modal children className

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,24 +2,24 @@ import { createPortal } from "react-dom";
 import { X } from "react-feather";
 interface props {
   children: JSX.Element;
+  childrenClassName?: string;
   small?: boolean;
 }
 
-const Modal = ({ children, small }: props) => {
+const Modal = ({ children, childrenClassName = "", small }: props) => {
   const modalEl = small ? null : document.getElementById("Modal");
 
   const modalContent: JSX.Element = (
     <div className="absolute top-0 z-[100] h-full w-full">
       <div
-        className={`h-full w-full backdrop-blur-md ${
-          small ? "rounded-[32px]" : ""
-        } bg-[#151515]/[.5]`}
+        className={`h-full w-full backdrop-blur-md ${small ? "rounded-[32px]" : ""
+          } bg-[#151515]/[.5]`}
       />
       <span className="absolute top-6 right-6 cursor-pointer text-white">
         <X size={20} />
       </span>
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-[300px] bg-white">{children}</div>
+        <div className={childrenClassName}>{children}</div>
       </div>
     </div>
   );
