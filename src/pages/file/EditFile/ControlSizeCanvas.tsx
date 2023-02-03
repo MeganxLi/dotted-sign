12c516e4532602bf49b089256401c433913c23e5
Meiyi <ant19950601@gmail.com>
2023-02-03 22:52:53 +0800

    :bug: Click outside the div to close

--- src/pages/file/EditFile/ControlSizeCanvas.tsx
+++ src/pages/file/EditFile/ControlSizeCanvas.tsx
@@ -4,68 +4,55 @@ import { ChevronDown, Minus, Plus } from "react-feather";
 const sizeOption: number[] = [25, 50, 75, 100];
 
 const ControlSizeCanvas = () => {
+  const dropdownRef = useRef<HTMLElement>(null);
   const [openMenu, setOpenMenu] = useState<boolean>(false);
-  const dropdownRef = useRef<HTMLUListElement>(null);
-
-  const clickMenu = () => {
-    console.log("click");
-
-    setOpenMenu(!openMenu);
-  };
 
   useEffect(() => {
-    console.log("openMenu", openMenu);
-  }, [openMenu]);
-
-  useEffect(() => {
-    const onClick = (e: MouseEvent) => {
+    const handleClickOutside = (e: MouseEvent) => {
       if (
         dropdownRef.current !== null &&
         !dropdownRef.current.contains(e.target as HTMLElement)
       ) {
-        console.log(!dropdownRef.current.contains(e.target as HTMLElement));
-
-        setOpenMenu(false);
+        setOpenMenu(!openMenu);
       }
     };
-
-    if (openMenu) {
-      console.log("openMenu uef", openMenu);
-
-      window.addEventListener("click", onClick);
-    }
+    document.addEventListener("click", handleClickOutside);
 
     return () => {
-      window.removeEventListener("click", onClick);
+      document.removeEventListener("click", handleClickOutside);
     };
-  }, [openMenu, dropdownRef]);
+  }, [dropdownRef]);
 
   return (
     <div
-      className="absolute right-3 bottom-2 flex cursor-pointer 
+      className="absolute right-3 bottom-2 flex 
     items-center gap-4 rounded-[8px] bg-white px-2 py-3 shadow-base"
     >
       <span
-        className="relative
-        after:absolute after:top-0 after:right-[-8px] after:block after:h-4 after:w-[1px] after:bg-black/20"
+        className="relative cursor-pointer after:absolute
+        after:top-0 after:right-[-8px] after:block after:h-4 after:w-[1px] after:bg-black/20 hover:text-blue"
       >
         <Plus size={16} />
       </span>
-      <span>
+      <span className="cursor-pointer hover:text-blue">
         <Minus size={16} />
       </span>
-      <span className="Roboto-Slab relative flex  text-right font-medium">
-        <span className="Roboto-Slab w-[40px]" onClick={clickMenu}>
-          10%
-        </span>
-        <span onClick={clickMenu}>
+      <span
+        className="Roboto-Slab relative flex text-right font-medium"
+        ref={dropdownRef}
+      >
+        <div
+          onClick={() => setOpenMenu(!openMenu)}
+          className="flex cursor-pointer hover:text-blue"
+        >
+          <span className="Roboto-Slab w-[40px]">10%</span>
           <ChevronDown size={16} />
-        </span>
+        </div>
+
         <ul
-          className={`absolute bottom-8 rounded-[8px] bg-white p-3 shadow-base ${
+          className={`absolute bottom-8 cursor-pointer rounded-[8px] bg-white p-3 shadow-base ${
             openMenu ? "block" : "hidden"
           }`}
-          ref={dropdownRef}
         >
           {sizeOption.map((size: number) => (
             <li
